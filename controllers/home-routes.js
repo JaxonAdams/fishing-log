b97b61d5df7a4d4e6c0c347bafd16f7b64b522ee
Jaxon Adams <jadamsresume1@gmail.com>
2022-05-03 16:38:30 -0600

    homepage now displays recently caught fish

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
+const { FishCaught } = require('../models');
 const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    FishCaught.findAll()
+    .then(dbFishData => {
+        const fish = dbFishData.map(fish => fish.get({ plain: true }));
+        console.log(fish);
+        res.render('homepage', { fish });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
